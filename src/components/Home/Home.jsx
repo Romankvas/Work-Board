fc4b4decc46a860edcdc774a9c166e14bb4a3b66
Roman Kvas <rk.sambir@gmail.com>
2026-06-06 14:15:19 +0300

    fff

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
 
-export default function Home() {
+const fetchPosts = async () => {
+  // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch("https://kndx3cr6-3000.euw.devtunnels.ms/api/products");
+  if (!response.ok) throw new Error("Помилка завантаження!");
+  return response.json();
+};
+
+function Posts() {
+   const { data, error, isLoading } = useQuery(
+        {
+            queryKey: ['posts'],
+            queryFn: fetchPosts
+        }
+    )
+
+  if (isLoading) return <p>⏳ Завантаження...</p>;
+  if (error) return <p>❌ Помилка: {error.message}</p>;
+
   return (
-    <div>
-      
-    </div>
-  )
+    <ul>
+      {data.map(post => (
+        <li key={post.id}>{post.name} - {post.price}</li>
+      ))}
+    </ul>
+  );
 }
+
+
+export default Posts;
